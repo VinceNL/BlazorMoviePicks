43c1c38670558444411772797b56399d3b22fef1
vvancampen <vvancampen@msn.com>
2024-04-15 21:50:20 +0200

    Setup API Service and connection

---
+++ BlazorMoviePicks/Services/TMDBClient.cs
@@ -0,0 +1,26 @@
+﻿using BlazorMoviePicks.Models;
+using System.Net.Http.Json;
+
+namespace BlazorMoviePicks.Services
+{
+    public class TMDBClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public TMDBClient(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+
+            _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+            _httpClient.DefaultRequestHeaders.Accept.Add(new("application/json"));
+
+            string apiKey = config["TMDBKey"] ?? throw new Exception("TMDBKey not found");
+            _httpClient.DefaultRequestHeaders.Authorization = new("Bearer", apiKey);
+        }
+
+        public Task<PopularMoviesPagedResponse?> GetPopularMoviesAsync()
+        {
+            return _httpClient.GetFromJsonAsync<PopularMoviesPagedResponse>("movie/popular");
+        }
+    }
+}
