f28a1b3a6bda58f811a6b50ffc483f5b9b12ce9b
vvancampen <vvancampen@msn.com>
2024-04-25 23:45:38 +0200

    Implented persisting the selected movies to local storage

--- BlazorMoviePicks/Program.cs
+++ BlazorMoviePicks/Program.cs
@@ -2,6 +2,7 @@ using BlazorMoviePicks;
 using BlazorMoviePicks.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<TMDBClient>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
